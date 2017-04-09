db6bcc0fe39ceef1e83d6c1d02c127abdffc60f1
Andrew Bonner <andrewbonner92@gmail.com>
2017-04-09 20:18:11 +0000

    add initialize method and self.all method

--- lib/tripadvisor_best/museum.rb
+++ lib/tripadvisor_best/museum.rb
@@ -1,2 +1,17 @@
 class TripAdvisorBest::Museum
+  attr_accessor :name, :location, :ranking
+  @@all = []
+
+  def initialize(attributes)
+    attributes.each{|k, v| self.send(("#{k}="), v)}
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_attributes()
+
+  end
 end
