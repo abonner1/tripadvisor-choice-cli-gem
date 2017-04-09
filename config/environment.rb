bc4c0c8f81afa4c5f5fd56e32613b4659430de1e
Andrew Bonner <andrewbonner92@gmail.com>
2017-04-09 16:11:51 +0000

    add require_relatvies for museum, attraction, and landmark classes

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require 'nokogiri'
 require 'open-uri'
 
 require_relative '../lib/tripadvisor_best/cli'
+require_relative '../lib/tripadvisor_best/museum'
+require_relative '../lib/tripadvisor_best/attraction'
+require_relative '../lib/tripadvisor_best/landmark'
