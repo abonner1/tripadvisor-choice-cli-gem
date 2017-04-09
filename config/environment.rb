70dfb34a1b29f21ae773bc4b59e1eb0ebeadf79f
Andrew Bonner <andrewbonner92@gmail.com>
2017-04-09 16:44:06 +0000

    add pry and nokogiri requirements to gemspec, and update environment.rb and tripadvisor_best to reflect change

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
-require 'pry'
-require 'nokogiri'
 require 'open-uri'
+require 'nokogiri'
+require 'pry'
 
 require_relative '../lib/tripadvisor_best/cli'
 require_relative '../lib/tripadvisor_best/museum'
