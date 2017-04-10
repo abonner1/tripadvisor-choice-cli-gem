4f84b14499105d8a95f1c9acd82eeb5e1c8bb949
Andrew Bonner <andrewbonner92@gmail.com>
2017-04-10 22:19:15 +0000

    add scrap_details_page method

--- lib/tripadvisor_best/scraper.rb
+++ lib/tripadvisor_best/scraper.rb
@@ -1,22 +1,35 @@
 class TripAdvisorBest::Scraper
 
-  def get_page(site)
-    Nokogiri::HTML(open(site))
+  def get_page(url)
+    Nokogiri::HTML(open(url))
   end
 
-  def scrape_listings_page(site)
-    page = self.get_page(site)
-    place_array = []
+  def scrape_listings_page(url)
+    page = self.get_page(url)
+    highlights_array = []
     page.css("#WINNERVIEWER div.posRel.tcInner").each do |e|
-      place = {
+      highlight = {
         :name => e.css(".mainName a").text,
         :location => e.css(".winnerName .smaller a").text,
         :ranking => e.css("div.posn span").text,
-        :url => e.css(".mainName a").attribute("href").value
+        :url => "https://www.tripadvisor.com/#{e.css(".mainName a").attribute("href").value}"
       }
 
-      place_array << place
+      highlights_array << highlight
     end
-    place_array
+    highlights_array
+  end
+
+  def scrap_details_page(url)
+    page = self.get_page(url)
+    highlight_details = {}
+    binding.pry
+    page.css("div.main_section.listingbar").each do |e|
+      highlight_details[:address] = e.css(".format_address span").text
+      highlight_details[:hours] = e.css(".hours_wrapper div.time").text
+      highlight_details[:visit_length] = e.css(".details_wrapper div.detail").first.text
+      highlight_details[:description] = e.css(".details_wrapper div.listing_details p").text
+    end
+    highlight_details
   end
 end
