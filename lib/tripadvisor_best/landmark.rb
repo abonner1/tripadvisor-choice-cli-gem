2f28437261f999b6903091f199244ddac6dd52ee
Andrew Bonner <andrewbonner92@gmail.com>
2017-04-10 01:53:08 +0000

    add inheritence to Landmark class

--- lib/tripadvisor_best/landmark.rb
+++ lib/tripadvisor_best/landmark.rb
@@ -1,21 +1,15 @@
-class TripAdvisorBest::Landmark
+require_relative 'place'
+
+class TripAdvisorBest::Landmark < TripAdvisorBest::Place
   attr_accessor :name, :location, :ranking, :url
   @@all = []
 
   def initialize(attributes)
-    attributes.each{|k, v| self.send(("#{k}="), v)}
+    super
     self.class.all << self
   end
 
   def self.all
     @@all
-  end
-
-  def self.create_from_collection(landmarks_array)
-    landmarks_array.each{|landmark| self.new(landmark)}
-  end
-
-  def add_landmark_attributes(attributes)
-    attributes.each{|k, v| self.send(("#{k}="), v)}
   end
 end
