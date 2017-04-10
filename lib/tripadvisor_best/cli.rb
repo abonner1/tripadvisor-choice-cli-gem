ca390d9e1e1d6c676217fa807c791754a4370340
Andrew Bonner <andrewbonner92@gmail.com>
2017-04-10 23:08:58 +0000

    add colorization to list and front load class data

--- lib/tripadvisor_best/cli.rb
+++ lib/tripadvisor_best/cli.rb
@@ -21,9 +21,11 @@ class TripAdvisorBest::CLI
   end
 
   def add_highlight_details
+    puts "It's gonna be great!"
     self.sites.each do |site|
-      site[:name].all.each do |highlight|
-        TripAdvisorBest::Scraper.new.scrap_details_page(highlight.url)
+      site[:class_name].all.each do |highlight|
+        attributes = TripAdvisorBest::Scraper.new.scrap_details_page(highlight.url)
+        highlight.add_highlight_attributes(attributes)
       end
     end
   end
@@ -47,14 +49,13 @@ class TripAdvisorBest::CLI
       case input
       when "1"
         puts "Here are the top 25 Museums in the world..."
-        list_top_level(sites[0][:class_name])
-        #see_more_details(sites[0][:class_name])
+        list_hightlights(sites[0][:class_name])
       when "2"
         puts "Here are the top 25 Attractions in the world..."
-        list_top_level(sites[1][:class_name])
+        list_hightlights(sites[1][:class_name])
       when "3"
         puts "Here are the top 25 Landmarks in the world..."
-        list_top_level(sites[2][:class_name])
+        list_hightlights(sites[2][:class_name])
       when "list"
         list_options
       when "exit"
@@ -82,25 +83,15 @@ class TripAdvisorBest::CLI
   def make_highlights
     puts "Planning your next big trip..."
     self.sites.each do |site|
-      objects_array = []
-      objects_array = TripAdvisorBest::Scraper.new.scrape_listings_page(site[:url])
-      site[:class_name].create_from_collection(objects_array)
+      highlights_array = []
+      highlights_array = TripAdvisorBest::Scraper.new.scrape_listings_page(site[:url])
+      site[:class_name].create_from_collection(highlights_array)
     end
   end
 
-  def list_top_level(class_name)
+  def list_hightlights(class_name)
     class_name.all.each do |highlight|
-      puts "#{highlight.ranking}. - #{highlight.name} - #{highlight.location}"
+      puts "#{highlight.ranking}. - #{highlight.name}".colorize(:yellow)
     end
   end
-
-  def see_more_details(class_name)
-    puts "Which one would you like to see more details?"
-    input = gets.strip
-
-    highlight = class_name.find(input-1)
-
-    show_highlight(highlight)
-  end
-
 end
