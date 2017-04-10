f6b4e9d2586e7e1bc597abc2690772ef56916f24
Andrew Bonner <andrewbonner92@gmail.com>
2017-04-10 20:37:18 +0000

    transfer requirements from config to tripadvisor_best.rb

--- config/environment.rb
+++
@@ -1,9 +0,0 @@
-require 'open-uri'
-require 'nokogiri'
-require 'pry'
-
-require_relative '../lib/tripadvisor_best/cli'
-require_relative '../lib/tripadvisor_best/museum'
-require_relative '../lib/tripadvisor_best/attraction'
-require_relative '../lib/tripadvisor_best/landmark'
-require_relative '../lib/tripadvisor_best/scraper'
--- lib/tripadvisor_best.rb
+++ lib/tripadvisor_best.rb
@@ -1,4 +1,14 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+require 'tripadvisor_best/cli'
+
+require 'tripadvisor_best/version'
+require 'tripadvisor_best/museum'
+require 'tripadvisor_best/attraction'
+require 'tripadvisor_best/landmark'
+require 'tripadvisor_best/scraper'
+
 module TripAdvisorBest
 end
-
-require_relative "../config/environment"
